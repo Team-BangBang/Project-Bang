c4bcfaf3134af3486e476d31acc3414daeef081e
David Danilchik <davdd@mac.com>
2023-08-03 22:10:30 -0700

    got buttons working on about page, started styling

---
+++ aboutus/aboutus.js
@@ -0,0 +1,15 @@
+'strict';
+
+
+const scoreBoardButton = document.getElementById('score-board-button');
+const playAgainButton = document.getElementById('ret-urn-button');
+
+
+playAgainButton.addEventListener('click', function() {
+  window.location.href = '/index.html';
+});
+
+scoreBoardButton.addEventListener('click', function() {
+  window.location.href = '/scoreboard/scoreboard.html';
+});
+
